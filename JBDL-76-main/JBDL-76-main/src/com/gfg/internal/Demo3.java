3c7310a01553db43fec759be0cb901aa8bf2ef13
GeeksforgeeksDS <courses@geeksforgeeks.org>
2024-09-25 12:09:21 +0530

    Add files via upload

---
+++ JBDL-76-main/JBDL-76-main/src/com/gfg/internal/Demo3.java
@@ -0,0 +1,12 @@
+package com.gfg.internal;
+
+import com.gfg.inheritance.Calculator;
+
+public class Demo3 extends Calculator {
+
+
+    public static void main(String[] args) {
+        Demo3 demo3 = new Demo3();
+        demo3.getA();
+    }
+}
---
+++ JBDL-76-main/JBDL-76-main/src/com/gfg/internal/Main.java
@@ -0,0 +1,8 @@
+package com.gfg.internal;
+
+public class Main {
+    public static void main(String[] args) {
+        Demo3 demo3 = new Demo3();
+//        System.out.println(demo3.);
+    }
+}
